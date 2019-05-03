9566330084131155eeb7aff5beb8c5321c6b89ad
vagrant <vagrant@homestead>
2019-05-03 07:03:43 +0000

    Fixed folder structure

---
+++ src/SuggestionBoxServiceProvider.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Shervs336\SuggestionBox;
+
+use Illuminate\Support\ServiceProvider;
+
+class SuggestionBoxServiceProvider extends ServiceProvider
+{
+  public function boot()
+  {
+    $this->loadMigrationsFrom(__DIR__.'/../migrations');
+
+    $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
+
+    $this->loadViewsFrom(__DIR__.'/../resources/views', 'suggestion-box');
+
+    $this->publishes([
+      __DIR__.'/../resources/views' => base_path('resources/views/vendor/suggestion-box'),
+    ]);
+  }
+
+  public function register()
+  {
+
+  }
+}
